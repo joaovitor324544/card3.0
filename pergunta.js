a3130fe04f0d37e30890cc71b81bdfaf612c2f31
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2024-11-19 10:56:51 +0000

    dado

---
+++ pergunta.js
@@ -0,0 +1,57 @@
+criaCartao{
+    'Matemática'
+    'qual é o valor de x em x/22+3=18'
+    '330'
+}
+criaCartao{
+    'Português'
+    'Na frase "João passou no concurso"o sujeito é'
+    'João'
+}
+criaCartao{
+    'ingles'
+    'traduza a frase:It’s pouring, take an umbrella.'
+    'Está chovendo muito, leve um guarda-chuva.'
+}
+criaCartao{
+    'história'
+    'quem foi o lider do movimento nazista'
+    'Adolf hitler'
+}
+criaCartao{
+    'sociologia'
+    'Émile Durkheim , as três principais características do Fato Social são:'
+    'generalidade, a exterioridade e a coercitividade.'
+}
+criaCartao{
+    'geografia'
+    'a parte final do rio, caracterizada pela abertura larga sem acúmulo de sedimentos'
+    'estuário '
+}
+criaCartao{
+    'educação financeira'
+    'processo de estabilização econômica iniciada em 1993'
+    'plano real'
+}
+criaCartao{
+    'quimica'
+    'de nomenclatura desse hidreto?'
+    'pentino '
+}
+criaCartao{
+    'mat Li'
+    'qual tag é usada para representar o cabeçario'
+    'header'
+}
+criaCartao{
+    'biologia'
+    'quem é o pai da genetica?'
+    'Mendel'
+}
+criaCartao{
+    'Física'
+    'Se a energia cinética média das moléculas de um gás aumentar e o volume permanecer constante'
+    'pressão e a temperatura aumentarão.'
+}
+
+
