bea919f8200b8ba64d049f978e19593c7a7dda43
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2024-11-26 10:31:27 +0000

    26/11

--- pergunta.js
+++ pergunta.js
@@ -1,61 +1,75 @@
 criaCartao(
-'Matemática',
-'qual é o valor de x em x/22+3=18',
-'330'
+    'Matemática',
+    'qual é o valor de x em x/22+3=18',
+    '330'
 )
-    
 
-criaCartao(  
-'Português'
-    'Na frase "João passou no concurso"o sujeito é'
+
+criaCartao(
+    'Português',
+    'Na frase "João passou no concurso"o sujeito é',
     'João'
 )
 
 
-criaCartao{
-    'ingles'
-    'traduza a frase:It’s pouring, take an umbrella.'
+criaCartao(
+
+    'ingles',
+    'traduza a frase:It’s pouring, take an umbrella.',
     'Está chovendo muito, leve um guarda-chuva.'
-}
-criaCartao{
-    'história'
-    'quem foi o lider do movimento nazista'
+
+)
+
+criaCartao(
+    'história',
+    'quem foi o lider do movimento nazista',
     'Adolf hitler'
-}
-criaCartao{
-    'sociologia'
-    'Émile Durkheim , as três principais características do Fato Social são:'
+)
+
+criaCartao(
+    'sociologia',
+    'Émile Durkheim , as três principais características do Fato Social são:',
     'generalidade, a exterioridade e a coercitividade.'
-}
-criaCartao{
-    'geografia'
-    'a parte final do rio, caracterizada pela abertura larga sem acúmulo de sedimentos'
+)
+
+criaCartao(
+    'geografia',
+    'a parte final do rio, caracterizada pela abertura larga sem acúmulo de sedimentos',
     'estuário '
-}
-criaCartao{
-    'educação financeira'
-    'processo de estabilização econômica iniciada em 1993'
+)
+
+
+criaCartao(
+    'educação financeira',
+    'processo de estabilização econômica iniciada em 1993',
     'plano real'
-}
-criaCartao{
-    'quimica'
-    'de nomenclatura desse hidreto?'
+
+)
+
+criaCartao(
+    'quimica',
+    'de nomenclatura desse hidreto?',
     'pentino '
-}
-criaCartao{
-    'mat Li'
-    'qual tag é usada para representar o cabeçario'
+)
+
+criaCartao(
+    'mat Li',
+    'qual tag é usada para representar o cabeçario',
     'header'
-}
-criaCartao{
-    'biologia'
-    'quem é o pai da genetica?'
+)
+criaCartao(
+    'biologia',
+    'quem é o pai da genetica?',
     'Mendel'
-}
-criaCartao{
-    'Física'
-    'Se a energia cinética média das moléculas de um gás aumentar e o volume permanecer constante'
+)
+
+
+criaCartao(
+    'Física',
+    'Se a energia cinética média das moléculas de um gás aumentar e o volume permanecer constante',
     'pressão e a temperatura aumentarão.'
-}
+)
+
+
 
 
